32212648f8e40bdddbf13cb963a615a3d798b619
srfrmac87 <48164725+srfrmac87@users.noreply.github.com>
2024-08-06 13:55:52 -0400

    Update ACPress-Paywall.user.js

--- ACPress-Paywall.user.js
+++ ACPress-Paywall.user.js
@@ -1,60 +1,50 @@
 // ==UserScript==
-// @name         AC Press Paywall Remover
+// @name         CMC Herald Paywall Remover
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0.2
-// @description  Fixed on 8/9/23 for AC Press
+// @version      1.0.0.2
+// @description  Updated 8/6/24 for fixing Herald paywall and removing AC Press paywall
 // @author       You
-// @match        *.pressofatlanticcity.com/*
-// @match        *pressofatlanticcity.com/*
-// @icon         https://www.google.com/s2/favicons?domain=pressofatlanticcity.com
+// @match        *.capemaycountyherald.com/*
+// @match        *capemaycountyherald.com/*
+// @icon         https://www.google.com/s2/favicons?domain=capemaycountyherald.com
 // @grant        GM_addStyle
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @downloadURL  https://github.com/srfrmac87/ACPress/raw/main/ACPress-Paywall.user.js
-// @updateURL    https://github.com/srfrmac87/ACPress/raw/main/ACPress-Paywall.user.js
-// @run-at        document-start
+// @downloadURL  https://github.com/srfrmac87/CMCHerald/raw/main/CMCHerald-Paywall.user.js
+// @updateURL    https://github.com/srfrmac87/CMCHerald/raw/main/CMCHerald-Paywall.user.js
 // ==/UserScript==
-
 
 (function() {
     'use strict';
-    var $ = window.jQuery;
-    var timerVar = setInterval (function() {Greasemonkey_main (); }, 10);
+
+    var timerVar = setInterval (function() {Greasemonkey_main (); }, 125);
     var trueA = false;
     var trueB = false;
-    var trueC = false;
-    var trueD = false;
-    var trueE = false;
     var counter = 0;
 
     function Greasemonkey_main () {
-        $("#asset-content  .lee-article-text").removeClass('lee-article-text'); // script on AC website removes html from this class, to prevent, we remove class
-        $("#asset-content .inline-asset").removeClass('inline-asset'); // script on AC website removes html from this class, to prevent, we remove class
-
-        if($('.modal-backdrop.in').length){
-            $('.modal-backdrop.in').css("display", "none");
-            trueA=true;
+        if(location.hostname.match('capemaycountyherald')){
+            if(document.getElementById("piano-wall")){
+				document.getElementById("piano-wall").remove();// this removes paywall box that covers the article in-line *****updated 8/6/2024*****
+				trueA=true;
+			}
+            if(trueB!=true){
+                var paragraphs = document.getElementsByTagName("p");
+                for(var i = 0; i < paragraphs.length; i++)// this selects all paragraph tags <p> then removes the hidden attributes from it *****updated 8/6/2024*****
+                {
+                    paragraphs[i].removeAttribute("hidden");
+                }
+                trueB=true;
+            }
+            counter++;
+            if(trueA==true && trueB==true){ // sets clearInterval for when it completes sucessfully
+                clearInterval (timerVar);
+                timerVar = "";
+            }
+            if (counter==480){ // sets clearInterval timeout for this number in seconds...60 seconds = 480 cycles/8 times per second for above 125 ms refresh
+                clearInterval (timerVar);
+                timerVar = "";
+            }
+        } else {
+            alert("This should never appear. If it does, tell Kyle.");
         }
-        if($('div#lee-subscription-wall').length){
-            $('div#lee-subscription-wall').css("display", "none");
-            trueB=true;
-        }
-        if($('.modal-backdrop.fade.in').length){
-            $('.modal-backdrop.fade.in').css("display", "none");
-            trueC=true;
-        }
-        if($('.modal-open').length){
-            $('.modal-open').css("overflow", "unset");
-            trueD=true;
-        }
-
-        counter++;
-        if(trueA==true && trueB==true && trueC==true && trueD==true){ // sets clearInterval for when it completes sucessfully
-            clearInterval (timerVar);
-            timerVar = "";
-        }
-        if (counter==400){ // sets clearInterval time out for this number in seconds / 100 (for above 10 ms refresh)
-            clearInterval (timerVar);
-            timerVar = "";
-        }
-
-}})();
+     }
+})();
